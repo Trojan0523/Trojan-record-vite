3cbb501b964ceb44b05f3d314fb9e9cd4cfd4bb5
trojan0523 <1153532327@qq.com>
2020-12-01 17:08:02 +0800

    feature:create component Tabs (without functionality, just style)

---
+++ src/components/Tabs.vue
@@ -0,0 +1,41 @@
+<template>
+  <ul class="tabs">
+    <li class="tabs-item" v-for="item in dataSource" :key="item.value" :class="liClass(item)" @click="select(item)">
+      {{ item.text }}
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({})
+</script>
+
+<style lang="scss" scoped>
+  .tabs {
+    background: #c4c4c4;
+    display: flex;
+    text-align: center;
+    font-size: 24px;
+    &-item {
+      width: 50%;
+      line-height: 64px;
+      height: 64px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+      
+      &.selected::after {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 4px;
+        background: #333333;
+      }
+    }
+  }
+</style>
